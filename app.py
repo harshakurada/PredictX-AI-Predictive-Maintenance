cc906b702d63c5b1ac2a6696ccaa2fec711f7092
harshakurada <harsha.kurada25@gmail.com>
2026-06-23 19:46:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 st.set_page_config(
     page_title="PredictX",
@@ -13,9 +14,15 @@ st.title("⚙️ PredictX")
 def load_data():
     return pd.read_csv("fleet_data.csv")
 
+@st.cache_resource
+def load_model():
+    return joblib.load("predictx_best_model.pkl")
+
 fleet = load_data()
 
-st.write("Application Loaded Successfully")
+model = load_model()
+
+st.success("Model Loaded Successfully")
 
 st.write("Fleet Shape:")
 st.write(fleet.shape)
